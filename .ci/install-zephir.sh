a30625999f09cef7e128b031f0168db19ca9142b
Ark4ne <gui.allegret@gmail.com>
2020-05-30 01:34:57 +0200

    try use older zephir version

--- .ci/install-zephir.sh
+++ .ci/install-zephir.sh
@@ -51,14 +51,14 @@ fi
 popd 1>/dev/null || exit 1
 
 mkdir -p "$HOME/bin"
-if [[ ! $ZEPHIR_VERSION =~ ^(master|development)$ ]]; then
-  wget \
-    --no-clobber \
-    -O "$HOME/bin/zephir" \
-    "https://github.com/phalcon/zephir/releases/download/$ZEPHIR_VERSION/zephir.phar"
-  chmod +x "$HOME/bin/zephir"
-  exit 0
-fi
+#if [[ ! $ZEPHIR_VERSION =~ ^(master|development)$ ]]; then
+#  wget \
+#    --no-clobber \
+#    -O "$HOME/bin/zephir" \
+#    "https://github.com/phalcon/zephir/releases/download/$ZEPHIR_VERSION/zephir.phar"
+#  chmod +x "$HOME/bin/zephir"
+#  exit 0
+#fi
 
 git clone -b "$ZEPHIR_VERSION" --depth 1 -q https://github.com/phalcon/zephir
 pushd zephir 1>/dev/null || exit 1
